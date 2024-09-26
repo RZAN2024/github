55ee4cf438e74828d29ea5948decea339dfbed3d
razan <rzan17897@gmail.com>
2024-09-26 17:02:42 +0300

    Initial Commit

---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by razan alkhider on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
